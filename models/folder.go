2f6be11256aae3e6dccda1a87fc5d301344c73e6
adcrn <9167686+adcrn@users.noreply.github.com>
2018-09-04 22:49:42 -0400

    Started mocking home page, working on tests and registration next

--- models/folder.go
+++ models/folder.go
@@ -4,9 +4,22 @@ import (
     "time"
 )
 
-type Folder struct {
+type folder struct {
     modelImpl
+    Owner        string     // Should be user's randomly-generated hash
     FolderName   string
     Created      time.Date
     NumElements  int
+}
+
+// Creating fake folders until for testing
+var folderList = []folder{
+    folder{Owner: "nna24ga9zn2haa", FolderName: "terns", 
+           Created: time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC), NumElements: 12},
+    folder{Owner: "plabs42yabn6sd", FolderName: "2014 Milan", 
+           Created: time.Date(2014, 09, 12, 08, 17, 42, 793654, time.UTC), NumElements: 27},
+}
+
+func getUsersFolders() []folder {
+    return folderList
 }
