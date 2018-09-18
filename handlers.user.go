c318429009a2ae1b1423510feccbb63433b91c0a
adcrn <9167686+adcrn@users.noreply.github.com>
2018-09-17 23:23:07 -0400

    Switched CSS framework to Spectre, added basic registration page

--- handlers.user.go
+++ handlers.user.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
 
 func showRegistrationPage(c *gin.Context) {
-    //
+    c.HTML(
+
+        http.StatusOK,
+
+        "register.html",
+
+        gin.H{
+            "title": "Register - knest",
+        },
+    )
 }
 
 func register(c *gin.Context) {
