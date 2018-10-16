106131458a2f5728d2dfcb537eb8e3c6db36d4cc
adcrn <9167686+adcrn@users.noreply.github.com>
2018-10-16 14:35:29 -0400

    Finished business logic for folder storage

--- models/folder.go
+++ models/folder.go
@@ -16,68 +16,9 @@ type Folder struct {
 	Downloaded    bool      `json:"downloaded"`
 }
 
-// FolderStorage is the interface through which methods will access the database
-// in order to operate on folder objects.
-type FolderStorage interface {
-	List(...FolderFilter) ([]Folder, error)
-	Get(...FolderFilter) (Folder, error)
-	Create(Folder) error
-	Update(Folder) error
-	Delete(...FolderFilter) error
-}
-
-// FolderFilter is the set of criteria that will be used to select certain
-// folders
-type FolderFilter func(*FolderFilterConfig) error
-
-// FolderFilterConfig is the struct that will be edited and then called by the
-// FolderFilter interface for searching.
-type FolderFilterConfig struct {
-	OwnerID    int
-	FolderName string
-	Created    time.Time
-	Completed  bool
-	Downloaded bool
-}
-
-// FolderOwnerIDFilter sets the ownerID field
-func FolderOwnerIDFilter(ownerID int) FolderFilter {
-	return func(fc *FolderFilterConfig) error {
-		fc.OwnerID = ownerID
-		return nil
-	}
-}
-
-// FolderFolderNameFilter sets the folderName field
-func FolderFolderNameFilter(folderName string) FolderFilter {
-	return func(fc *FolderFilterConfig) error {
-		fc.FolderName = folderName
-		return nil
-	}
-}
-
-// FolderCreatedFilter sets the created field
-func FolderCreatedFilter(created time.Time) FolderFilter {
-	return func(fc *FolderFilterConfig) error {
-		fc.Created = created
-		return nil
-	}
-}
-
-// FolderCompletedFilter sets the completed field
-func FolderCompletedFilter(completed bool) FolderFilter {
-	return func(fc *FolderFilterConfig) error {
-		fc.Completed = completed
-		return nil
-	}
-}
-
-// FolderDownloadedFilter sets the downloaded field
-func FolderDownloadedFilter(downloaded bool) FolderFilter {
-	return func(fc *FolderFilterConfig) error {
-		fc.Downloaded = downloaded
-		return nil
-	}
+type FolderUpdate struct {
+	Completed  bool `json:"completed"`
+	Downloaded bool `json:"downloaded"`
 }
 
 // Creating fake folders until for testing
--- storage/folder_store.go
+++ storage/folder_store.go
@@ -2,10 +2,8 @@ package storage
 
 import (
 	"database/sql"
-	//"errors"
 	"kne.st/models"
-	//"strings"
-	//"time"
+	"time"
 
 	_ "github.com/lib/pq" // Driver for database/sql
 )
@@ -15,8 +13,8 @@ import (
 type FolderStorage interface {
 	ListByUser(int) ([]models.Folder, error)
 	GetByName(int, string) (models.Folder, error)
-	Create(models.Folder) error
-	Update(models.Folder) error
+	Create(models.Folder) (int, error)
+	Update(models.Folder, models.FolderUpdate) error
 	Delete(int, string) error
 }
 
@@ -82,17 +80,90 @@ func (fs *FolderStore) GetByName(ownerID int, name string) (models.Folder, error
 }
 
 // Create takes in a folder object and creates a database record
-func (fs *FolderStore) Create(folder models.Folder) error {
-	return nil
+func (fs *FolderStore) Create(f models.Folder) (int, error) {
+	var folderID int
+	const sqlTimeFormat = "1993-01-05 23:45:00"
+
+	tx, err := fs.db.Begin()
+	if err != nil {
+		return -1, err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(`insert into folders(folder_name, upload_time, 
+					num_elements, completed, downloaded) values($1, $2, $3, $4, $5 RETURNING id`)
+	if err != nil {
+		return -1, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(f.FolderName, time.Now().Format(sqlTimeFormat),
+		f.NumElements, f.Completed, f.Downloaded).Scan(&folderID)
+	if err != nil {
+		return -1, err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return -1, nil
+	}
+
+	return folderID, nil
 }
 
 // Update should only be used to update the completed and downloaded fields
-func (fs *FolderStore) Update(folder models.Folder) error {
+func (fs *FolderStore) Update(f models.Folder, up models.FolderUpdate) error {
+
+	tx, err := fs.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(`update folders set completed = $1, downloaded = $2 where id = $3`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(up.Completed, up.Downloaded, f.OwnerID)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Delete takes in a user Id and folder name and removes the folder record
 func (fs *FolderStore) Delete(ownerID int, name string) error {
+
+	tx, err := fs.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(`delete from folders where id = $1 and folder_name = $2`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(ownerID, name)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
